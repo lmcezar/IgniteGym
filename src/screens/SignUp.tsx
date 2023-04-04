27d100c03c8c1792dc0a86f45fe9707475f7af57
Lucas Cezar <lmcezar@gmail.com>
2023-04-04 14:13:09 -0300

    Aplicando navegação de autenticação

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
 
 import LogoSvg from "@assets/logo.svg";
@@ -7,6 +8,12 @@ import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
 export function SignUp() {
+  const navigation = useNavigation();
+
+  function handleGoBack() {
+    navigation.goBack();
+  }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
@@ -43,7 +50,12 @@ export function SignUp() {
           <Button title="Criar e acessar" />
         </Center>
 
-        <Button mt={24} title="Voltar para o login" variant="outline" />
+        <Button
+          mt={24}
+          title="Voltar para o login"
+          variant="outline"
+          onPress={handleGoBack}
+        />
       </VStack>
     </ScrollView>
   );
